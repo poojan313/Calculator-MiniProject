975320847a71d0ce8d84981b8aaa2de6c03e3e20
Poojan Khatri <Poojan.Khatri@iiitb.org>
2021-03-12 18:14:10 +0530

    Used Scanner and remove new lines

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,36 +3,36 @@ import java.io.*;
 public class Calculator {
     public static void main(String[] args) throws IOException {
         Calculations calculations = new Calculations();
-        System.out.println("Enter what would you like to do\n1.Square Root\n2.Factorial\n3.Natural Logarithm\n4.Power Function\nEnter your choice: ");
-//        Scanner scanner = new Scanner(System.in);
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int choice = Integer.parseInt(br.readLine());
+        System.out.print("Enter what would you like to do\n1.Square Root\n2.Factorial\n3.Natural Logarithm\n4.Power Function\nEnter your choice: ");
+        Scanner sc = new Scanner(System.in);
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int choice = sc.nextInt();
         double result,num1,num2;
         long factorial,fact_input;
         switch (choice){
             case 1:
-                System.out.println("Enter the number: ");
-                num1 = Double.parseDouble(br.readLine());
+                System.out.print("Enter the number: ");
+                num1 = sc.nextDouble();
                 result = calculations.squareRoot(num1);
                 System.out.println("The square root is: "+Double.toString(result));
                 break;
             case 2:
-                System.out.println("Enter the number: ");
-                fact_input = Long.parseLong(br.readLine());
+                System.out.print("Enter the number: ");
+                fact_input = sc.nextLong();
                 factorial = calculations.factorial(fact_input);
                 System.out.println("The factorial is: "+Long.toString(factorial));
                 break;
             case 3:
-                System.out.println("Enter the number: ");
-                num1 = Double.parseDouble(br.readLine());
+                System.out.print("Enter the number: ");
+                num1 = sc.nextDouble();
                 result = calculations.naturalLog(num1);
                 System.out.println("The natural log is: "+Double.toString(result));
                 break;
             case 4:
-                System.out.println("Enter the number: ");
-                num1 = Double.parseDouble(br.readLine());
-                System.out.println("Enter the power: ");
-                num2 = Double.parseDouble(br.readLine());
+                System.out.print("Enter the number: ");
+                num1 = sc.nextDouble();
+                System.out.print("Enter the power: ");
+                num2 = sc.nextDouble();
                 result = calculations.powerFunction(num1,num2);
                 System.out.println("The power function value is: "+Double.toString(result));
                 break;
